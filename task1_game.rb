2e13330e4fc6bbb1072a3b8bf86e8aadac4e84b0
Pablo Chaburkin <chaburkin.pasha1999@gmail.com>
2021-04-04 11:02:41 +0300

    first

---
+++ task1_game.rb
@@ -0,0 +1,11 @@
+# Вгадай моє число
+# Написана мною
+puts "Welcome to 'Get My Number!'"
+
+#Отримання імя Гравця
+print "What's your name? "
+input = gets
+name = input.chomp
+
+puts "Welcome #{name}!"
+
